ed58b6d2fe8d5f154ae1160d4decb20ef0f24a06
Mihail Reznichenko <mihail.reznichenko.98@mail.ru>
2017-06-27 21:59:31 +0700

    add ViewSubscriber and comSubs in AbsPresenter

--- app/src/main/java/io/github/rezmike/foton/ui/abstracts/AbstractPresenter.kt
+++ app/src/main/java/io/github/rezmike/foton/ui/abstracts/AbstractPresenter.kt
@@ -5,6 +5,12 @@ import io.github.rezmike.foton.ui.activities.root.RootActivity
 import io.github.rezmike.foton.ui.activities.root.RootPresenter
 import mortar.MortarScope
 import mortar.ViewPresenter
+import rx.Observable
+import rx.Subscriber
+import rx.Subscription
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import rx.subscriptions.CompositeSubscription
 import javax.inject.Inject
 
 abstract class AbstractPresenter<V : AbstractView<P, V>, M : AbstractModel, P : AbstractPresenter<V, M, P>> : ViewPresenter<V>() {
@@ -14,6 +20,8 @@ abstract class AbstractPresenter<V : AbstractView<P, V>, M : AbstractModel, P : 
     @Inject
     protected lateinit var model: M
 
+    protected lateinit var compSubs: CompositeSubscription
+
     override fun onEnterScope(scope: MortarScope) {
         super.onEnterScope(scope)
         initDagger(scope)
@@ -21,7 +29,13 @@ abstract class AbstractPresenter<V : AbstractView<P, V>, M : AbstractModel, P : 
 
     override fun onLoad(savedInstanceState: Bundle?) {
         super.onLoad(savedInstanceState)
+        compSubs = CompositeSubscription()
         initActionBar()
+    }
+
+    override fun dropView(view: V) {
+        if (compSubs.hasSubscriptions()) compSubs.unsubscribe()
+        super.dropView(view)
     }
 
     abstract fun initDagger(scope: MortarScope)
@@ -30,5 +44,23 @@ abstract class AbstractPresenter<V : AbstractView<P, V>, M : AbstractModel, P : 
 
     fun getRootView(): RootActivity? {
         return rootPresenter.getRootView()
+    }
+
+    protected abstract inner class ViewSubscriber<T> : Subscriber<T>() {
+        override fun onCompleted() {
+            getRootView()?.hideLoad()
+        }
+
+        override fun onError(e: Throwable) {
+            getRootView()?.hideLoad()
+            getRootView()?.showError(e)
+        }
+    }
+
+    protected fun <T> subscribe(observable: Observable<T>, subscriber: ViewSubscriber<T>): Subscription {
+        return observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(subscriber)
     }
 }
