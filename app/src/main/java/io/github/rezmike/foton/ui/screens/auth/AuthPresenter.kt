e944a92afc732896166a5030dc006207b59d6d6f
Mihail Reznichenko <mihail.reznichenko.98@mail.ru>
2017-06-28 23:13:43 +0700

    implement AuthView and AuthPresenter

--- app/src/main/java/io/github/rezmike/foton/ui/screens/auth/AuthPresenter.kt
+++ app/src/main/java/io/github/rezmike/foton/ui/screens/auth/AuthPresenter.kt
@@ -1,6 +1,12 @@
 package io.github.rezmike.foton.ui.screens.auth
 
+import android.view.MenuItem
+import flow.Flow
+import io.github.rezmike.foton.R
 import io.github.rezmike.foton.ui.abstracts.AbstractPresenter
+import io.github.rezmike.foton.ui.activities.root.MenuItemHolder
+import io.github.rezmike.foton.ui.screens.login.LoginScreen
+import io.github.rezmike.foton.ui.screens.register.RegisterScreen
 import io.github.rezmike.foton.utils.DaggerService
 import mortar.MortarScope
 
@@ -11,6 +17,21 @@ class AuthPresenter : AbstractPresenter<AuthView, AuthModel, AuthPresenter>() {
     }
 
     override fun initActionBar() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        rootPresenter.ActionBarBuilder()
+                .setTitle(view?.resources?.getString(R.string.auth_title)!!)
+                .setOverFlowIcon(R.drawable.ic_custom_menu_black_24dp)
+                .addAction(MenuItemHolder(view?.context?.getString(R.string.auth_menu_login), 0, { onClickLogin() }, MenuItem.SHOW_AS_ACTION_NEVER))
+                .addAction(MenuItemHolder(view?.context?.getString(R.string.auth_menu_register), 0, { onClickRegister() }, MenuItem.SHOW_AS_ACTION_NEVER))
+                .build()
+    }
+
+    fun onClickLogin(): Boolean {
+        Flow.get(view).set(LoginScreen())
+        return true
+    }
+
+    fun onClickRegister(): Boolean {
+        Flow.get(view).set(RegisterScreen())
+        return true
     }
 }
